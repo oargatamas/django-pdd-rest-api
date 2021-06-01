e55bcb6c927478143b53ad14d9a48487df6ca921
toarga <oarga.tamas@gmail.com>
2021-06-01 22:47:06 +0200

    Basic price paid data model implemented.

--- ppd_rest_api/house_price_paid_data_search/models.py
+++ ppd_rest_api/house_price_paid_data_search/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class PricePaidData(models.Model):
+    transactionUniqueIdentifier = models.CharField()
+    price = models.DecimalField()
+    dateOfTransfer = models.DateTimeField()
+    postCode = models.CharField()
+    propertyType = models.CharField()
+    oldOrNew = models.BooleanField()
+    duration = models.CharField()
+    paon = models.CharField()
+    saon = models.CharField()
+    street = models.CharField()
+    locality = models.CharField()
+    city = models.CharField()
+    district = models.CharField()
+    county = models.CharField()
+    category = models.CharField()
+    status = models.CharField()
+
