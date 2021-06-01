aee0fe093487ed285d2e4e018b1342982b2e785b
toarga <oarga.tamas@gmail.com>
2021-06-01 22:24:25 +0200

    Django project init.

---
+++ ppd_rest_api/house_price_paid_data_search/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HousePricePaidDataSearchConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'house_price_paid_data_search'
