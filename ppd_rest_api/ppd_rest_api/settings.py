8ef03a0a10a522fe1d286c3c2c76f57fb16b17a9
toarga <oarga.tamas@gmail.com>
2021-06-02 14:41:08 +0200

    Rest framework added.

--- ppd_rest_api/ppd_rest_api/settings.py
+++ ppd_rest_api/ppd_rest_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'house_price_paid_data_search.apps.HousePricePaidDataSearchConfig',
 ]
 
