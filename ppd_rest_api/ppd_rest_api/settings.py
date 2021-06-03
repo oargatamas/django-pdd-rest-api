daafe037b7dd1c3a2c32ad2ea1b2b26b9a7bce50
toarga <oarga.tamas@gmail.com>
2021-06-03 10:19:28 +0200

    Swagger ui provided and setup correctly

--- ppd_rest_api/ppd_rest_api/settings.py
+++ ppd_rest_api/ppd_rest_api/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-#&zktq1=dm81z&@=xb&_cg(8f37hjbmyydvpup*crr!co(j+g%
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,11 +71,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ppd_rest_api.wsgi.application'
 
-
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer'
     ]
+}
+
+SWAGGER_SETTINGS = {
+    'DEFAULT_INFO': 'import.path.to.urls.api_info',
 }
 
 # Database
@@ -89,7 +90,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -109,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -123,7 +122,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -134,7 +132,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 PAGE_SIZE = 10
-DATE_FORMAT= "%Y-%m-%d %H:%M"
+DATE_FORMAT = "%Y-%m-%d %H:%M"
 API_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
 LOCAL_CSV_FILE_URI = 'C:/Users/oarga/Downloads/pp-complete.csv'
 LATEST_CSV_URL = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-monthly-update-new-version.csv"
--- ppd_rest_api/ppd_rest_api/urls.py
+++ ppd_rest_api/ppd_rest_api/urls.py
@@ -21,22 +21,22 @@ from drf_yasg import openapi
 from rest_framework import permissions
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="House Price Paid Data API",
-      default_version='v1',
-      description="Tech challenge for implement PPD REST API in Django",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="oarga.tamas@gmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="House Price Paid Data API",
+        default_version='v1',
+        description="Tech challenge for implement PPD REST API in Django",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="oarga.tamas@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    urlconf="house_price_paid_data_search.urls"
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("ppd/",include("house_price_paid_data_search.urls")),
+    path("ppd/", include("house_price_paid_data_search.urls")),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
