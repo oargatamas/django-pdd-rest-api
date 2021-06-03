4483b84902e4198d651fe0d2c4e859937200e948
toarga <oarga.tamas@gmail.com>
2021-06-03 11:57:07 +0200

    Test settings added. Triing to mock the csv repository

---
+++ ppd_rest_api/house_price_paid_data_search/test_settings.py
@@ -0,0 +1,5 @@
+LATEST_CSV_URL = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-monthly-update-new-version.csv"
+LOCAL_DATE_FILTERED_CSV_FILE_URI = "/samples/sample_date_filtered.csv"
+LOCAL_CSV_FILE_URI = "/samples/sample_all.csv"
+
+PAGE_SIZE = 5
--- ppd_rest_api/house_price_paid_data_search/tests.py
+++ ppd_rest_api/house_price_paid_data_search/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+import mock
+from rest_framework.test import RequestsClient
 
 # Create your tests here.
+from .repositories import FileSystemCachedCsvPpdRepository
+from . import test_settings
+
+
+def repositoryMock():
+    return FileSystemCachedCsvPpdRepository(fileUrl=test_settings.LOCAL_CSV_FILE_URI)
+
+
+class PpdApiTest(TestCase):
+    def setUp(self):
+        pass
+
+    @mock.patch("house_price_paid_data_search.repositories.get_repository", side_effect=repositoryMock)
+    def test_ppd_all_view_expect_all_record_with_paging(self):
+        print(self.repository.fileUrl)
+
+        self.assertTrue(True)
