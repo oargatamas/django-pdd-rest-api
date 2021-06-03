bf4d5090776ed012e659ff095c21d52707def836
toarga <oarga.tamas@gmail.com>
2021-06-03 08:16:23 +0200

    drf-yasg view added

--- ppd_rest_api/ppd_rest_api/settings.py
+++ ppd_rest_api/ppd_rest_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'house_price_paid_data_search.apps.HousePricePaidDataSearchConfig',
 ]
 
